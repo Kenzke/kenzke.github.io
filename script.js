d1cd4ec2e55d63e1262b7dee310219c112e823d8
Kenzke <65712328+Kenzke@users.noreply.github.com>
2023-02-07 14:14:08 +0100

    pageindexer

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ var drop = document.getElementById("drop");
 var cont = document.getElementById("content");
 var curr = document.getElementById("curr");
 var ele = document.getElementById("cont");
-var topl = document.getElementById("topl");
-var topr = document.getElementById("topr");
+var left = document.getElementById("topl_id");
+var right = document.getElementById("topr_id");
+//index of number of pages to cycle thru
+var numberOfPages=8;
 
+var path = window.location.pathname;
+
+var page = path.split("/").pop();
+
+var pagestring = page.split(".")[0];
+
+
+if(pagestring=="index"){
+    pagestring="1";
+}
+
+var pagenumber = parseInt(pagestring, 10);
 
 ele.onclick = function() {
     curr.style.display="block";
@@ -20,3 +34,20 @@ drop.onclick = function() {
         curr.style.display="none";
     }
 };
+
+right.onclick = function(){
+    if(pagenumber>=numberOfPages){
+        location.href = "index.html";
+    }else{
+        var pagenumberplusone = pagenumber + 1;
+        location.href = pagenumberplusone+".html";
+    }
+}
+left.onclick = function(){
+    if(pagenumber==1){
+        window.open(numberOfPages+".html");
+    }else{
+        var pagenumberminusone = pagenumber -1;
+        window.open(pagenumberminusone+".html");
+    }
+}
